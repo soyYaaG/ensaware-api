fe52a6d463970b4e771a8cc388dcb7af42e0815b
Yerson Alexander Arredondo García <yerson-302@hotmail.com>
2023-09-17 00:16:41 -0500

    create exception

---
+++ utils/exception/ensaware.py
@@ -0,0 +1,31 @@
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+
+
+class EnsawareException(Exception):
+    def __init__(self, status_code: int, type: str, message: str) -> None:
+        self.message = message
+        self.status_code = status_code
+        self.type = type
+
+        super().__init__(self.message)
+
+
+class EnsawareExceptionBase(BaseModel):
+    code: int
+    message: str
+    type: str
+
+
+class EnsawareExceptionHandler:
+    def ensaware(self, enw: EnsawareException):
+        enw_base = EnsawareExceptionBase(
+            code=enw.status_code,
+            message=enw.message,
+            type=enw.type
+        )
+
+        return JSONResponse(
+            status_code=enw.status_code,
+            content=enw_base.model_dump()
+        )
