df8ae0fcdb68381963382723758fd96ae6cba548
Yerson Alexander Arredondo García <yerson-302@hotmail.com>
2023-09-17 00:29:00 -0500

    add sql file

---
+++ utils/sql/create.sql
@@ -0,0 +1,127 @@
+/*
+  _____                                        
+ | ____|_ __  ___  __ ___      ____ _ _ __ ___ 
+ |  _| | '_ \/ __|/ _` \ \ /\ / / _` | '__/ _ \
+ | |___| | | \__ \ (_| |\ V  V / (_| | | |  __/
+ |_____|_| |_|___/\__,_| \_/\_/ \__,_|_|  \___|
+                                               
+*/
+
+-- ------------
+-- extension --
+-- ------------
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+-- ----------------------
+-- Create career table --
+-- ----------------------
+CREATE TABLE IF NOT EXISTS career (
+	id UUID DEFAULT UUID_GENERATE_V4(),
+	name VARCHAR(100) NOT NULL,
+	is_active BOOLEAN NOT NULL DEFAULT TRUE,
+	created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	modified TIMESTAMP NULL,
+	CONSTRAINT pk_career_id PRIMARY KEY (id),
+	CONSTRAINT unq_career_name UNIQUE (name)
+);
+
+
+-- -----------------------
+-- Create profile table --
+-- -----------------------
+CREATE TABLE IF NOT EXISTS profile (
+	id UUID DEFAULT UUID_GENERATE_V4(),
+	name VARCHAR(100) NOT NULL,
+	is_active BOOLEAN NOT NULL DEFAULT TRUE,
+	created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	modified TIMESTAMP NULL,
+	CONSTRAINT pk_profile_id PRIMARY KEY (id),
+	CONSTRAINT unq_profile_name UNIQUE (name)
+);
+
+
+-- --------------------
+-- Create user table --
+-- --------------------
+CREATE TABLE IF NOT EXISTS "user" (
+	id UUID DEFAULT UUID_GENERATE_V4(),
+	provider_id VARCHAR(60) NOT NULL,
+	provider VARCHAR(50) NOT NULL,
+	display_name VARCHAR(255) NOT NULL,
+	email VARCHAR(100) NOT NULL,
+	picture VARCHAR(255) NULL,
+	profile_id UUID NOT NULL,
+	career_id UUID NULL,
+	refresh_token VARCHAR(255) NOT NULL,
+	is_active BOOLEAN NOT NULL DEFAULT TRUE,
+	created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	modified TIMESTAMP NULL,
+	CONSTRAINT pk_user_id PRIMARY KEY (id),
+	CONSTRAINT unq_user_email UNIQUE (email),
+	CONSTRAINT fk_user_profile_id FOREIGN KEY (profile_id) REFERENCES profile (id),
+	CONSTRAINT fk_user_career_id FOREIGN KEY (career_id) REFERENCES career (id),
+	CONSTRAINT unq_user_refresh_token UNIQUE (refresh_token)
+);
+
+-- Create indexes
+CREATE INDEX IF NOT EXISTS idx_user_provider ON "user" (provider);
+
+
+-- ----------------------------
+-- Create content_type table --
+-- ----------------------------
+CREATE TABLE IF NOT EXISTS content_type (
+	id UUID DEFAULT UUID_GENERATE_V4(),
+	model VARCHAR(100) NOT NULL,
+	created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	modified TIMESTAMP NULL,
+	CONSTRAINT pk_content_type_id PRIMARY KEY (id),
+	CONSTRAINT unq_content_type_model UNIQUE (model)
+);
+
+
+-- --------------------
+-- Create permission --
+-- --------------------
+CREATE TABLE IF NOT EXISTS permission (
+	id UUID DEFAULT UUID_GENERATE_V4(),
+	content_type_id UUID NOT NULL,
+	code_name VARCHAR(255) NOT NULL,
+	description VARCHAR(100) NULL,
+	created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	modified TIMESTAMP NULL,
+	CONSTRAINT pk_permission_id PRIMARY KEY (id),
+	CONSTRAINT fk_permission_content_type_id FOREIGN KEY (content_type_id) REFERENCES content_type (id),
+	CONSTRAINT unq_permission_code_name UNIQUE (code_name)
+);
+
+
+-- ----------------------------
+-- Create permission_profile --
+-- ----------------------------
+CREATE TABLE IF NOT EXISTS permission_profile (
+	id UUID DEFAULT UUID_GENERATE_V4(),
+	permission_id UUID NOT NULL,
+	profile_id UUID NOT NULL,
+	created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	modified TIMESTAMP NULL,
+	CONSTRAINT pk_permission_profile_id PRIMARY KEY (id),
+	CONSTRAINT fk_permission_profile_profile_id FOREIGN KEY (profile_id) REFERENCES profile (id),
+	CONSTRAINT fk_permission_profile_permission_id FOREIGN KEY (permission_id) REFERENCES permission (id),
+	CONSTRAINT unq_permission_profile UNIQUE (permission_id, profile_id)
+);
+
+
+-- -- --------------------------------
+-- -- Create historic_qr_code table --
+-- -- --------------------------------
+-- CREATE TABLE IF NOT EXISTS historic_qr_code (
+-- 	id UUID DEFAULT UUID_GENERATE_V4(),
+-- 	user_id UUID NOT NULL,
+-- 	"data" TEXT NULL,
+-- 	is_active BOOLEAN NOT NULL DEFAULT TRUE,
+-- 	created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+-- 	modified TIMESTAMP NULL,
+-- 	CONSTRAINT pk_historic_qr_code_id PRIMARY KEY (id)
+-- 	CONSTRAINT fk_historic_qr_code_user_id FOREIGN KEY (user_id) REFERENCES "user" (id),
+-- );
---
+++ utils/sql/insert.sql
@@ -0,0 +1,55 @@
+/*
+  _____                                        
+ | ____|_ __  ___  __ ___      ____ _ _ __ ___ 
+ |  _| | '_ \/ __|/ _` \ \ /\ / / _` | '__/ _ \
+ | |___| | | \__ \ (_| |\ V  V / (_| | | |  __/
+ |_____|_| |_|___/\__,_| \_/\_/ \__,_|_|  \___|
+                                               
+*/
+
+-- -----------------
+-- Insert profile --
+-- -----------------
+INSERT INTO profile (name)
+    SELECT 'estudiante' UNION
+    SELECT 'profesor' UNION
+    SELECT 'administrador' UNION
+    SELECT 'super_administrador';
+
+
+-- ----------------
+-- Insert career --
+-- ----------------
+INSERT INTO career (name)
+    SELECT 'Administración de Empresas' UNION
+    SELECT 'Ingeniería de Sistemas' UNION
+    SELECT 'Ingeniería Industrial' UNION
+    SELECT 'Licenciatura en Pedagogía de la Primera Infancia' UNION
+    SELECT 'Negocios Internacionales' UNION
+    SELECT 'Psicología' UNION
+    SELECT 'Técnica Profesional en Procesos Contables' UNION
+    SELECT 'Técnica Profesional en Procesos Logísticos y de Comercio Exterior' UNION
+    SELECT 'Técnica Profesional en Procesos Turísticos y Hoteleros' UNION
+    SELECT 'Derecho';
+
+
+-- ----------------------
+-- Insert content_type --
+-- ----------------------
+INSERT INTO content_type (model)
+    SELECT 'permission' UNION
+    SELECT 'user';
+
+
+-- --------------------
+-- Insert permission --
+-- --------------------
+INSERT INTO permission (content_type_id, code_name, description)
+    SELECT id, CONCAT(model, ':create'), 'Crear permisos' FROM content_type WHERE model = 'permission' UNION
+    SELECT id, CONCAT(model, ':read'), 'Leer permisos' FROM content_type WHERE model = 'permission' UNION
+    SELECT id, CONCAT(model, ':update'), 'Editar permisos' FROM content_type WHERE model = 'permission' UNION
+    SELECT id, CONCAT(model, ':delete'), 'Eliminar permisos' FROM content_type WHERE model = 'permission' UNION
+    SELECT id, CONCAT(model, ':create'), 'Crear Usuarios' FROM content_type WHERE model = 'user' UNION
+    SELECT id, CONCAT(model, ':read'), 'Leer Usuarios' FROM content_type WHERE model = 'user' UNION
+    SELECT id, CONCAT(model, ':update'), 'Editar Usuarios' FROM content_type WHERE model = 'user' UNION
+    SELECT id, CONCAT(model, ':delete'), 'Eliminar Usuarios' FROM content_type WHERE model = 'user';
