9cc36bc51e07f707c7b9495d2e8f9c35a1259c69
Yerson Alexander Arredondo García <yerson-302@hotmail.com>
2023-11-05 20:27:33 -0500

    feat: get info about me

--- user/v1/router.py
+++ user/v1/router.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from utils.database import get_db
 from utils.oauth.security import Security
 
+from authorization.v1.schema import TokenData
 from user.v1.crud import DBUser
 from user.v1.schema import UserRead
 
@@ -18,6 +19,24 @@ router = APIRouter(
 get_db = router.dependencies[0]
 get_token = router.dependencies[1]
 db_user = DBUser
+
+
+@router.get(
+    '/',
+    response_model=UserRead,
+    status_code=status.HTTP_200_OK,
+)
+async def get_user(
+    db: Session = get_db,
+    token: TokenData = get_token,
+):
+    '''
+    Obtener la información del usuario que inicio sesión.
+
+    ### Return
+    - `UserRead` Respuesta con la información del usuario
+    '''
+    return await db_user(db).get_user_id(token.sub, True)
 
 
 @router.get(
