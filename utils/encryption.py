6e3e945305c7c2e5d9fea6fd4bdd82406f3d4ce7
Yerson Alexander Arredondo García <yerson-302@hotmail.com>
2023-11-06 22:35:09 -0500

    refactor: change variables

--- utils/encryption.py
+++ utils/encryption.py
@@ -6,14 +6,12 @@ from utils.settings import Settings
 class Encryption:
     def __init__(self) -> None:
         self._settings: Settings = Settings()
-        self._fernet_pass: str = self._settings.fernet_pass
-        self._fernet: Fernet = Fernet(self._fernet_pass)
-        self._encode: str = self._settings.encode
+        self._fernet: Fernet = Fernet(self._settings.fernet_pass)
 
 
     def encrypt(self, message: str) -> str:
-        return self._fernet.encrypt(message.encode(self._encode)).decode(self._encode)
-    
+        return self._fernet.encrypt(message.encode(self._settings.encode)).decode(self._settings.encode)
+
 
     def decrypt(self, token: str) -> str:
-        return self._fernet.decrypt(token).decode(self._encode)
+        return self._fernet.decrypt(token).decode(self._settings.encode)
--- utils/oauth/security.py
+++ utils/oauth/security.py
@@ -11,17 +11,13 @@ from utils.exception.ensaware import EnsawareException
 from utils.settings import Settings
 from user.v1.schema import User
 
-
-settings = Settings()
 oauth2_token = HTTPBearer(auto_error=False)
 
 
 class Security:
     def __init__(self) -> None:
+        self.__settings: Settings = Settings()
         self.__algorithm = 'HS512'
-        self.__expire_minutes = settings.jwt_expire_minutes
-        self.__encode = settings.encode
-        self.__secret_key = settings.jwt_secret_key
         self.__utc = datetime.now(timezone.utc)
 
     def jwt_encode(self, user: User) -> Token:
@@ -31,7 +27,7 @@ class Security:
         }
 
         now: datetime = self.__utc.now()
-        exp: datetime = now + timedelta(minutes=self.__expire_minutes)
+        exp: datetime = now + timedelta(minutes=self.__settings.jwt_expire_minutes)
 
         payload: dict = {
             'email': user.email,
@@ -43,7 +39,7 @@ class Security:
 
         try:
             token: str = jwt.encode(
-                header, payload, self.__secret_key).decode(self.__encode)
+                header, payload, self.__settings.jwt_secret_key).decode(self.__settings.encode)
 
             return Token(
                 token=token,
@@ -55,7 +51,7 @@ class Security:
 
     def jwt_decode(self, token: str) -> TokenData:
         try:
-            payload: dict = jwt.decode(token, self.__secret_key)
+            payload: dict = jwt.decode(token, self.__settings.jwt_secret_key)
             unix = int(self.__utc.now().timestamp())
 
             if unix > payload['exp']:
@@ -71,8 +67,9 @@ class Security:
 
     @staticmethod
     def get_token(token: Annotated[str, Depends(oauth2_token)]) -> TokenData:
-        security = Security()
         if not token:
             raise EnsawareException(
                 status.HTTP_403_FORBIDDEN, TypeMessage.VALIDATION.value, Message.INVALID_JWT.value)
+
+        security = Security()
         return security.jwt_decode(token.credentials)
