b2a92a79f8bf627013a3dd716dc8ad498feda5c3
Yerson Alexander Arredondo García <yerson-302@hotmail.com>
2023-11-13 02:19:54 -0500

    feat: add update_career_id

--- user/v1/crud.py
+++ user/v1/crud.py
@@ -6,7 +6,7 @@ from sqlalchemy.future import select
 from sqlalchemy.orm import selectinload, Session
 
 from user.v1.models import UserModel
-from user.v1.schema import UserBase, User, UserRead
+from user.v1.schema import UserBase, User, UserRead, UserUpdate
 
 from utils import Message, TypeMessage
 from utils.exception.ensaware import EnsawareException
@@ -18,7 +18,7 @@ class DBUser:
         self.__session = session
 
 
-    def __select(self, return_user_read_model):
+    def __select(self, return_user_read_model: bool) -> UserRead | User:
         if return_user_read_model:
             return select(UserModel).options(
                 selectinload(UserModel.profile),
@@ -26,6 +26,14 @@ class DBUser:
             )
         else:
             return select(UserModel)
+
+
+    def __validate(self, user):
+        if not user:
+            raise EnsawareException(
+                status.HTTP_404_NOT_FOUND, TypeMessage.VALIDATION.value, Message.NO_INFORMATION.value)
+
+        return user
 
 
     async def add_user(self, user: UserBase, return_user_read_model: bool = False) -> UserRead | User:
@@ -41,9 +49,9 @@ class DBUser:
                 return UserRead.model_validate(add_user)
             else:
                 return User.model_validate(add_user)
-        except Exception as ex:
+        except:
             raise EnsawareException(
-                status.HTTP_500_INTERNAL_SERVER_ERROR, TypeMessage.ERROR.value, str(ex))
+                status.HTTP_500_INTERNAL_SERVER_ERROR, TypeMessage.ERROR.value, Message.ERROR_ADD_USER.value)
 
 
     async def get_user_id(self, id: str, return_user_read_model: bool = False) -> UserRead | User | None:
@@ -61,9 +69,9 @@ class DBUser:
                 return UserRead.model_validate(result)
             else:
                 return User.model_validate(result)
-        except Exception as ex:
+        except:
             raise EnsawareException(
-                status.HTTP_500_INTERNAL_SERVER_ERROR, TypeMessage.ERROR.value, str(ex))
+                status.HTTP_500_INTERNAL_SERVER_ERROR, TypeMessage.ERROR.value, Message.ERROR_GET_USER.value)
 
 
     async def get_user_email(self, email: str, return_user_read_model: bool = False) -> UserRead | User | None:
@@ -73,26 +81,23 @@ class DBUser:
 
             execute = await self.__session.execute(select_user)
             result = execute.scalars().first()
-
-            if not result:
-                return None
+            self.__validate(result)
 
             if return_user_read_model:
                 return UserRead.model_validate(result)
             else:
                 return User.model_validate(result)
-        except Exception as ex:
+        except EnsawareException as enw:
+            raise enw
+        except:
             raise EnsawareException(
-                status.HTTP_500_INTERNAL_SERVER_ERROR, TypeMessage.ERROR.value, str(ex))
+                status.HTTP_500_INTERNAL_SERVER_ERROR, TypeMessage.ERROR.value, Message.ERROR_GET_USER.value)
 
 
     async def update_user_id(self, id: str, update_user: User, return_user_read_model: bool = False) -> UserRead | User:
         try:
             select_user = await self.get_user_id(id)
-
-            if not (select_user):
-                raise EnsawareException(
-                    status.HTTP_404_NOT_FOUND, TypeMessage.VALIDATION.value, Message.NO_INFORMATION.value)
+            self.__validate(select_user)
 
             update_user.modified = DefaultValuesModels.utc()
 
@@ -103,6 +108,30 @@ class DBUser:
             await self.__session.commit()
 
             return await self.get_user_id(id, return_user_read_model)
-        except Exception as ex:
+        except EnsawareException as enw:
+            raise enw
+        except:
             raise EnsawareException(
-                status.HTTP_500_INTERNAL_SERVER_ERROR, TypeMessage.ERROR.value, str(ex))
+                status.HTTP_500_INTERNAL_SERVER_ERROR, TypeMessage.ERROR.value, Message.ERROR_UPDATE_USER.value)
+
+
+    async def update_career_id(self, id: str, update_career: UserUpdate, return_user_read_model: bool = False) -> UserRead | User:
+        try:
+            select_user = await self.get_user_id(id)
+            self.__validate(select_user)
+
+            select_user.modified = DefaultValuesModels.utc()
+            select_user.career_id = update_career.career_id
+
+            query_update = update(UserModel).execution_options(synchronize_session=False).filter(
+                UserModel.id == id, UserModel.is_active).values(select_user.model_dump())
+
+            await self.__session.execute(query_update)
+            await self.__session.commit()
+
+            return await self.get_user_id(id, return_user_read_model)
+        except EnsawareException as enw:
+            raise enw
+        except:
+            raise EnsawareException(
+                status.HTTP_500_INTERNAL_SERVER_ERROR, TypeMessage.ERROR.value, Message.ERROR_UPDATE_USER_CAREER.value)
